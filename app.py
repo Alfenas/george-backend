2254c4ad9232a37c982828bd973a3c3b6d1603bf
Jonathan Barbosa <jonathan@MacBook-Pro-de-Jonathan.local>
2022-05-28 16:27:13 -0300

    .

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from app import app
 from os import environ
 
-
 if __name__ == '__main__':
-    SERVER_HOST = environ.get('SERVER_HOST', '0.0.0.0')
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host=SERVER_HOST,port=port, debug=True,threaded=True)
-#(not environ.get('ENV') == 'PRODUCTION')
+    SERVER_HOST = environ.get('SERVER_HOST', 'localhost')
+    app.run(host=SERVER_HOST,port=5500, debug=(not environ.get('ENV') == 'PRODUCTION'),threaded=True)
